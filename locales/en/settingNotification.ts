1a4c2e84a30d84e300e061758a0d1b68e2d52a96
Muhammad Anova N <muhammadanovan@192.168.1.6>
2022-04-22 13:59:29 +0700

    request changes

--- locales/en/settingNotification.ts
+++ locales/en/settingNotification.ts
@@ -2,8 +2,8 @@ const locale = {
   "settingNotification": {
     "description": "You will get all notification from this store according to the medium you activated below",
     "emailDescription": "All notification by email cannot changed",
-    "title": "Notification",
-    "pleaseFillWANumber": "Please fill in your whatsapp number first"
+    "pleaseFillWANumber": "Please fill in your whatsapp number first",
+    "title": "Notification"
   }
 }
 
--- locales/id/settingNotification.ts
+++ locales/id/settingNotification.ts
@@ -2,8 +2,8 @@ const locale = {
   "settingNotification": {
     "description": "Anda akan mendapatkan semua notifikasi dari toko ini sesuai dengan media yang Anda aktifkan di bawah ini",
     "emailDescription": "Semua notifikasi melalui email tidak dapat diubah",
-    "title": "Notifikasi",
-    "pleaseFillWANumber": "Mohon untuk mengisi nomor whatsapp terlebih dahulu"
+    "pleaseFillWANumber": "Mohon untuk mengisi nomor whatsapp terlebih dahulu",
+    "title": "Notifikasi"
   }
 }
 
