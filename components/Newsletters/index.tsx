1b9687fb95e09684fd8f3cc056a31adc519d3f51
Muhammad Anova N <muhammadanovan@192.168.1.3>
2022-05-05 12:03:20 +0700

    feat: newsletter and maintenance mode

--- components/Newsletters/index.tsx
+++ components/Newsletters/index.tsx
@@ -2,30 +2,45 @@
 import { FC } from 'react'
 import { Newsletter } from '@sirclo/nexus'
 import { toast } from 'react-toastify'
-
+/* library template */
+import useWindowSize from 'lib/useWindowSize'
 /* component */
 import styles from 'public/scss/components/Newsletter.module.scss'
 
 const classesNewsletter = {
-  containerClassName: styles.newsletter_popupInner,
-  closeButtonClassName: styles.newsletter_close,
-  formContainerClassName: styles.newsletter_form,
-  labelClassName: styles.newsletter_label,
-  inputClassName: styles.newsletter_input,
-  buttonClassName: styles.newsletter_button,
+  newsLetterWrapperClassName: styles.newsletter_wrapper,
+  newsLetterContainerClassName: styles.newsletter_popupInner,
+  newsLetterCloseButtonClassName: styles.newsletter_close,
+  newsLetterContentContainerClassName: styles.newsletter_contentContainer,
+  newsLetterContentClassName: styles.newsletter_content,
+  newsLetterImageContainerClassName: styles.newsletter_imageContainer,
+  newsLetterImageClassName: styles.newsletter_image,
+  newsLetterNoThanksButtonClassName: styles.button,
+  newsLetterFormContainerClassName: styles.newsletter_form,
+  newsLetterFormLabelClassName: styles.newsletter_label,
+  newsLetterFormInputClassName: styles.newsletter_input,
+  newsLetterFormButtonClassName: styles.newsletter_button
 }
 
-const Newsletters: FC<any> = ({ i18n }) => {
+const Newsletters: FC<any> = ({
+  i18n
+}) => {
+  const size: any = useWindowSize()
 
   return (
     <div className={styles.newsletter_popupContainer}>
       <Newsletter
         classes={classesNewsletter}
-        closeButton={i18n.t("home.later")}
         withForm
+        noThanksButton={i18n.t("home.later")}
         buttonComponent={i18n.t("newsletter.subscribe")}
         onComplete={() => toast.success(i18n.t("newsletter.submitSuccess"))}
         onError={() => toast.error(i18n.t("newsletter.submitError"))}
+        thumborSetting={{
+          width: size.width < 768 ? 512 : 800,
+          format: 'webp',
+          quality: 85,
+        }}
       />
     </div>
   )
