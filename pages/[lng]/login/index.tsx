f370aa3c9d317e4d62d7fdd3f514fc694427a59d
unknown <anggara.diebrata@sirclo.com>
2022-08-03 14:56:52 +0700

    feat: nexus 2.14.2, shipper logo, recaptcha

--- pages/[lng]/login/index.tsx
+++ pages/[lng]/login/index.tsx
@@ -1,5 +1,5 @@
 /* library package */
-import { FC } from 'react'
+import { FC, useRef } from 'react'
 import { GetServerSideProps, InferGetServerSidePropsType } from 'next'
 import { toast } from 'react-toastify'
 import {
@@ -43,6 +43,7 @@ const LoginPage: FC<any> = ({
 
   const i18n: any = useI18n()
   const size = useWindowSize()
+  const recaptchaRef = useRef<any>()
 
   const icons = {
     passwordViewIcon: <Icon.register.passwordViewIcon />,
