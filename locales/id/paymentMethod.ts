150c4a2fc1998f19c4d39e6ec415625a8d123572
lukmannm <luckymanlukman19@gmail.com>
2023-01-03 15:01:58 +0700

    feat: revamp the order history page

--- locales/id/paymentMethod.ts
+++ locales/id/paymentMethod.ts
@@ -1,8 +1,18 @@
 const locale = {
-  "paymentMethod": {
-    "copySuccess": "Berhasil tersalin",
-    "downloadQrSuccess": "Berhasil Mengunduh Gambar"
+    "paymentMethod": {
+      "copySuccess": "Berhasil tersalin",
+      "counterInstruction": "Tunjukkan barcode ini ke kasir",
+      "downloadQrSuccess": "Berhasil Mengunduh Gambar",
+      "expiryDateWarning": "Bayar sebelum",
+      "manualBankTransfer": "Transfer Bank Manual",
+      "manualBankTransferInstruction": "Pilih 1 rekening penerima pembayaran",
+      "manualBankTransferAccountNamePrefix": "a.n.",
+      "page": "halaman pembayaran",
+      "qrDownload": "Unduh QR Code",
+      "qrInstruction": "Scan kode QR melalui aplikasi e-wallet atau m-banking yang mendukung pembayaran QRIS",
+      "seeMore": "Lihat info lengkap di",
+      "continuePayment": "Lanjutkan pembayaran"
+    }
   }
-}
-
-export default locale
+  
+  export default locale
