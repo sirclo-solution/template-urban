9444f367658aa8f9ed76af9b98e41e45c7189ae1
Mutiara A'yun <mutiara.ayun19@gmail.com>
2023-01-30 15:35:23 +0700

    Release prod 20230130

---
+++ locales/en/paymentMethod.ts
@@ -0,0 +1,8 @@
+const locale = {
+  "paymentMethod": {
+    "copySuccess": "Copied Successfully",
+    "downloadQrSuccess": "Image Downloaded Successfully"
+  }
+}
+
+export default locale
---
+++ locales/id/paymentMethod.ts
@@ -0,0 +1,8 @@
+const locale = {
+  "paymentMethod": {
+    "copySuccess": "Berhasil tersalin",
+    "downloadQrSuccess": "Berhasil Mengunduh Gambar"
+  }
+}
+
+export default locale
