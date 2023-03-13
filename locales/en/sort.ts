ceea308518e60f101fc5634b99da67142f78259d
muhammad.iqbal <muhammad.iqbal@sirclo.com>
2023-03-13 15:27:47 +0700

    fix: fix sort locales

--- locales/en/sort.ts
+++ locales/en/sort.ts
@@ -1,31 +1,13 @@
 const locale = {
-  "testimonial": {
-    "add": "Add Testimonial",
-    "createError": "Something went wrong! Create testimonial failed!",
-    "createSuccess": "Create testimonial success!",
-    "desc": "We try our best to leave a beautiful impression to our customers, the warm greetings of those who have been with us.",
-    "inputAuthor": "Author",
-    "inputAuthorPlaceholder": "Author name",
-    "inputCompany": "Company",
-    "inputCompanyPlaceholder": "Company name",
-    "inputContent": "Content",
-    "inputContentPlaceholder": "Content",
-    "inputEmail": "Email",
-    "inputEmailPlaceholder": "email",
-    "inputImage": "Image",
-    "inputTitle": "Title",
-    "inputTitlePlaceholder": "Title",
-    "isEmpty": "No Reviews",
-    "moreReview": "More Review",
-    "no": "No",
-    "publishTestimonial": "May we publish your testimonial on our website?",
-    "submitBtn": "Submit",
-    "successUpload": "Upload image success",
-    "title": "Testimonial",
-    "weHave": "We Have ",
-    "weHave2": " testimonials from our loyal customers",
-    "writeReview": "Write Review",
-    "yes": "Yes"
+  "sort": {
+    "aToZAscending": "Name (A-Z)",
+    "aToZDescending": "Name (Z-A)",
+    "by": "Sort by",
+    "createdAtAscending": "Date (Old to New)",
+    "createdAtDescending": "Date (New to Old)",
+    "default": "Reset",
+    "priceAscending": "Price (Low to High)",
+    "priceDescending": "Price (High to Low)"
   }
 }
 
