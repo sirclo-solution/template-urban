57b667b7089bbedf7c6d82becfce761808c415ba
Mutiara A'yun <mutiara.ayun19@gmail.com>
2023-09-21 18:01:57 +0700

    feat(gssp): Implement token integration in hooks

--- pages/[lng]/index.tsx
+++ pages/[lng]/index.tsx
@@ -202,6 +202,7 @@ export const getServerSideProps: GetServerSideProps = async ({
     res.end();
   }
 
+  console.log("gssp", token, brand, dataBanners);
   return {
     props: {
       ...brand,
